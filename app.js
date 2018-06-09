a0f15a90be51c41cb539ff9be6b27f2b858d6312
Daniel Feather <MrFevs@users.noreply.github.com>
2018-06-09 16:40:53 +0100

    Switched view engine to pug

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ if(!process.env.TRANSPORT_API_APPID){
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'pug');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
